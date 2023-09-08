9a730a22c2b3c3dc8bc812f71a8cc9b2b266aad8
akshaynarisetti <akshaynarisettia@gmail.com>
2023-09-08 11:37:26 +0530

    Added reclaimswiggy, same is implemented in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,21 +17,16 @@ class MainApp extends StatelessWidget {
       itemCount: 1, // Only one item
       itemBuilder: (BuildContext context, int index) {
         return Center(
-          child: ReclaimHttps(
+          child: ReclaimSwiggy(
   requestedProofs: [
-    RequestedProof(
-      url: 'https://www.amazon.in/',
-      loginUrl: 'https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0',
-      loginCookies: ['x-acbin', 'at-acbin', 'sess-at-acbin', 'sst-acbin', 'i18n-prefs', 'lc-acbin'],
-      responseSelections: [
-        ResponseSelection(
-          responseMatch: 'nav-prime-1',
-        ),
-      ],
+    SwiggyRequestedProof(
+      url: 'https://www.swiggy.com/dapi/order/all?order_id=',
+      loginUrl: 'https://www.swiggy.com/auth',
+      loginCookies: ['_session_tid'],
     ),
   ],
-  title: "Amazon Prime",
-  subTitle: "Prove you have an Amazon Prime membership",
+  title: "Swiggy",
+  subTitle: "Prove that you are a swiggy user",
   cta: "Prove",
   onSuccess: (proofs) {
     // do something
--- lib/reclaim_flutter.dart
+++ lib/reclaim_flutter.dart
@@ -6,6 +6,8 @@ import 'package:wallet/wallet.dart' as wallet;
 import 'package:flutter_ethers/flutter_ethers.dart';
 import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 
 // ignore: must_be_immutable
@@ -78,6 +80,7 @@ class WebViewScreen extends StatelessWidget {
 
         if(response["type"] == "error"){
           onModification('Claim Creation Failed');
+          // print(response);
           onFail(Exception("${response["data"]["message"]}"));
           Navigator.pop(context);
         }
@@ -231,6 +234,19 @@ class RequestedProof {
   });
 }
   
+
+class SwiggyRequestedProof {
+  final String url;
+  final String loginUrl;
+  final List<String> loginCookies;
+  
+  SwiggyRequestedProof({
+    required this.url,
+    required this.loginUrl,
+    required this.loginCookies,
+  });
+}
+
 class ResponseSelection {
   final String responseMatch;
 
@@ -510,3 +526,509 @@ Widget build(BuildContext context) {
     );
   }
 }  
+
+
+// ignore: must_be_immutable
+class ReclaimSwiggy extends StatefulWidget {
+  final List<SwiggyRequestedProof> requestedProofs;
+  final String title;
+  final String subTitle;
+  String cta;
+  final Function(Map<String, dynamic> proofs) onSuccess;
+  final Function(Exception e) onFail;
+
+  ReclaimSwiggy({
+    Key? key,
+    required this.requestedProofs,
+    required this.title,
+    required this.subTitle,
+    required this.cta,
+    required this.onSuccess,
+    required this.onFail,
+  }) : super(key: key);
+
+  @override
+  _ReclaimSwiggyState createState() => _ReclaimSwiggyState();
+}
+
+class _ReclaimSwiggyState extends State<ReclaimSwiggy> {
+
+  String _claimState = "";
+  
+  void _openWebView(BuildContext context, String url, List<SwiggyRequestedProof> requestedProofs, Function(Map<String, dynamic> proofs) onSuccess, Function(Exception e) onFail) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => SwiggyWebViewScreen(context: context, url: Uri.parse(url), requestedProofs: requestedProofs, onModification: (webViewData) {setState(() {
+              _claimState = webViewData; 
+            }); }, onSuccess: onSuccess, onFail: onFail),
+      ),
+    );
+  }
+    @override
+Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 358,
+          // height: 201,
+          clipBehavior: Clip.antiAlias,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                width: 0.50,
+                color: Colors.black.withOpacity(0.10000000149011612),
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            shadows: const [
+              BoxShadow(
+                color: Color(0x14000000),
+                blurRadius: 16,
+                offset: Offset(0, 4),
+                spreadRadius: 0,
+              )
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 358,
+                            height: 40,
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  left: 3.75,
+                                  top: 6.25,
+                                  child: Container(
+                                    width: 30,
+                                    height: 30,
+                                    decoration: const BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage("https://reclaim-react-native-sdk.s3.ap-south-1.amazonaws.com/Logomark.png"),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const Positioned(
+                                  left: 130,
+                                  top: 13,
+                                  child: SizedBox(
+                                    height: 16,
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          width: 322,
+                                          child: Text(
+                                            'Powered by Reclaim Protocol',
+                                            style: TextStyle(
+                                              color: Colors.black45,
+                                              fontSize: 13,
+                                              fontFamily: 'Manrope',
+                                              fontWeight: FontWeight.w500,
+                                              height: 1.23,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 48,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Text(
+                      widget.title,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w700,
+                        height: 1.20,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 16,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 322,
+                            child: Text(
+                              widget.subTitle,
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(0.6000000238418579),
+                                fontSize: 13,
+                                fontFamily: 'Manrope',
+                                fontWeight: FontWeight.w500,
+                                height: 1.23,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          _claimState.isEmpty ?  Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(),
+                child:  ClipRRect(
+  borderRadius: BorderRadius.circular(12),
+  child: Material(
+    color: const Color(0xFF322EED),
+    child: InkWell(
+      onTap: (){
+        _openWebView(context, widget.requestedProofs[0].loginUrl, widget.requestedProofs, widget.onSuccess, widget.onFail);
+      }, 
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: SizedBox( 
+              height: 48,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child:  Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.cta,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontFamily: 'Manrope',
+                            fontWeight: FontWeight.w700,
+                            height: 1.33,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  ),
+)
+              ) : Container( 
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: SizedBox(
+                      width: double.infinity,
+                      height: 16,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 322,
+                            child: Text(
+                              _claimState,
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(0.6000000238418579),
+                                fontSize: 13,
+                                fontFamily: 'Manrope',
+                                fontWeight: FontWeight.w500,
+                                height: 1.23,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class SwiggyWebViewScreen extends StatelessWidget {
+  BuildContext context;
+  Uri url;
+  List<SwiggyRequestedProof> requestedProofs;
+  final Function(String webViewData) onModification;
+  Function(Map<String, dynamic> proofs) onSuccess;
+  Function(Exception e) onFail;
+  // Create WebViewController
+  var controller = WebViewController();
+  final cookieManager = WebviewCookieManager();
+  late String cookieStr;
+  late dynamic parseResult;
+  late Timer timer;
+  bool oneTimeRun = false;
+  bool watchDog = false;
+  SwiggyWebViewScreen({Key? key,required this.context, required this.url, required this.requestedProofs, required this.onModification, required this.onSuccess, required this.onFail})
+      : super(key: key) {
+    // Configure WebViewController 
+    cookieManager.clearCookies();
+    controller
+      ..addJavaScriptChannel(
+  'Login',
+  onMessageReceived: (JavaScriptMessage message) {
+    // print(message.message);
+    parseResult = message.message;
+    // print("cookie str is ${cookieStr}");
+    controller.runJavaScript('''Claim.postMessage("Init")''');
+  },
+)
+    ..addJavaScriptChannel(
+      'Check',
+      onMessageReceived: (JavaScriptMessage message) {
+          
+          var response = jsonDecode(message.message);
+          // print(message.message);
+        if(response["type"] == "createClaimStep"){
+          if(response["step"]["name"] == "creating" ){
+            Fluttertoast.showToast(
+        msg: "Creating Claim",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 2,
+        textColor: Colors.white,
+        fontSize: 16.0,
+        backgroundColor: const Color.fromARGB(255, 86, 86, 86)
+        
+    );
+          onModification('Creating Claim');
+          }
+          if(response["step"]["name"] == "witness-done" ){
+            Fluttertoast.showToast(
+        msg: "Claim Created Successfully",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 2,
+        textColor: Colors.white,
+        fontSize: 16.0,
+        backgroundColor: const Color.fromARGB(255, 86, 86, 86)
+        
+    );
+          onModification('Claim Created Successfully');
+          }
+        }
+         if(response["type"] == "createClaimDone"){
+            // print("response reciveved");
+            // print(response);
+            onSuccess(response["response"]);
+            Navigator.pop(context);
+        }
+
+        if(response["type"] == "error"){
+          onModification('Claim Creation Failed');
+          onFail(Exception("${response["data"]["message"]}"));
+          Navigator.pop(context);
+        }
+
+      },
+    )
+      ..addJavaScriptChannel(
+  'Claim',
+  onMessageReceived: (JavaScriptMessage message) async{
+    // print("create Claim");
+    // print(message.message);
+    controller.loadRequest(Uri.parse("https://sdk-rpc.reclaimprotocol.org/"));
+    Fluttertoast.showToast(
+        msg: "Initiating Claim Creation",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 2,
+        textColor: Colors.white,
+        fontSize: 16.0,
+        backgroundColor: const Color.fromARGB(255, 86, 86, 86)
+        
+    );
+    onModification('Please wait, Initiating Claim Creation');
+  },
+)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+         NavigationDelegate(
+           onProgress: (int progress) {
+            //  print("Loading ${progress}%");
+           },
+           onPageFinished: (String url) async { 
+  // print("on page finished ${url}");
+if(url == "https://sdk-rpc.reclaimprotocol.org/"){
+    final mnemonic = wallet.generateMnemonic();
+    final walletMnemonic = Wallet.fromMnemonic(mnemonic.join(' '));
+    //  print(walletMnemonic.privateKey);
+    //  print(walletMnemonic.publicKey);
+    //  print(walletMnemonic.address);
+    Map<String, dynamic> jsonObject = jsonDecode(parseResult);
+    jsonObject.remove("csrfToken");
+    String updatedJsonString = jsonEncode(jsonObject);
+
+
+     Map<String, dynamic> req = {
+        "channel": "Check",
+        "module": "witness-sdk",
+        "id": "123",
+        "type": "createClaim",
+        "request": {
+          "name": "swiggy-equal",
+          "params": {
+            "userData": updatedJsonString
+          },
+          "secretParams": {
+            "cookieStr": cookieStr,
+          },
+          "ownerPrivateKey": walletMnemonic.privateKey,
+        }
+      };
+    // print(jsonEncode(req));
+    controller.runJavaScript('''postMessage(${jsonEncode(req)})''');
+  return;
+}
+
+  if(!oneTimeRun){ 
+    timer = Timer.periodic(const Duration(seconds: 2), (Timer t) async{
+    // print("what is up");
+    if(watchDog){
+      return;
+    }
+  final gotCookies = await cookieManager.getCookies(requestedProofs[0].loginUrl);
+  List<String> foundCookies = [];
+  bool found = requestedProofs[0].loginCookies.every((cookie) {
+    if (gotCookies.indexWhere((item) => item.name == cookie) != -1){
+      foundCookies.add(cookie);
+      return true;
+    }
+    return false;
+  });
+  // print(foundCookies);
+  if (found) {
+    watchDog = true;
+    timer.cancel();
+    cookieStr = gotCookies.map((c) => '${c.name}=${c.value}').join('; ');
+
+          final response = await http.get(
+    Uri.parse(requestedProofs[0].url),
+    headers: {
+      'Cookie': cookieStr,
+    },
+  );
+      if (response.statusCode == 200) {
+      //  print(jsonDecode(response.body));
+       parseResult = response.body;
+       controller.runJavaScript('''Claim.postMessage("Init")''');
+  } else {
+    Navigator.pop(context);
+    onFail(Exception('Failed to load JSON data from url'));
+    throw Exception('Failed to load JSON data from url');
+  }
+    
+  }
+  });
+  oneTimeRun = false;
+  }
+
+},
+         ),
+       )
+      ..loadRequest(url);
+      
+  }
+
+  Map<String, dynamic> parseHtml(String html, String regexString) {
+  // replace {{VARIABLE}} with (.*?), and save the variable names
+  List<String> variableNames = [];
+  String realRegexString = regexString.replaceAllMapped(
+    RegExp(r'{{(.*?)}}'),
+    (match) {
+      variableNames.add(match.group(1)!);
+      return '(.*?)';
+    },
+  );
+
+  // create a RegExp object
+  RegExp regex = RegExp(realRegexString, multiLine: true, dotAll: true);
+
+  // run the regex on the html
+  Match? match = regex.firstMatch(html);
+
+  if (match == null) {
+    // print('Regex does not match');
+    onFail(Exception("Regex does not match"));
+    Navigator.pop(context);
+    throw 'Regex does not match';
+  }
+
+  // replace the variable placeholders in the original regex string with their values
+  String result = regexString;
+  Map<String, dynamic> params = {};
+  for (int i = 0; i < variableNames.length; i++) {
+    result = result.replaceAll('{{${variableNames[i]}}}', match.group(i + 1)!);
+    params[variableNames[i]] = match.group(i + 1)!;
+  }
+
+  return {'result': result, 'params': params};
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
